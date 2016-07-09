6719023d62404ced63b44b2fa2a2537c4171c730
helfy <562812743@qq.com>
2016-07-09 10:58:45 +0800

    修改

---
+++ HFLibs/HFAdditions/UIViewController+HFExtend.h
@@ -0,0 +1,16 @@
+//
+//  UIViewController+HFExtend.h
+//  HFKitDemo
+//
+//  Created by helfy on 16/6/27.
+//  Copyright © 2016年 helfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (HFExtend)
+
+
+- (UIViewController *)hf_pushToViewControllerForName:(NSString *)className;
+
+@end
